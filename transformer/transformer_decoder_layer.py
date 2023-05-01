12a76a952123bdca5935ba7f2fe6f8313a6d6e8a
Grozby <gabriele.aldeghi.27@gmail.com>
2023-05-01 13:42:49 +0100

    feat(transformer): add decoder layer

---
+++ transformer/transformer_decoder_layer.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+
+from transformer.multi_head_attention import MultiHeadAttention
+from transformer.transformer import TransformerEncoderLayer
+
+
+class TransformerDecoderLayer(TransformerEncoderLayer):
+
+    def __init__(
+        self,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(*args, **kwargs)
+        self.masked_multi_attention_head = MultiHeadAttention(
+            model_dim=self.model_dim,
+            number_attention_heads=self.number_attention_heads,
+            dropout_rate=self.dropout_rate,
+        )
+        self.masked_attention_layer_norm = nn.LayerNorm(self.model_dim)
+
+    def forward(self, x: torch.Tensor, x_decoder: torch.Tensor):
+        masked_attention = self.masked_multi_attention_head(
+            queries=x,
+            keys=x,
+            values=x,
+        )
+        x = self.masked_attention_layer_norm(x + masked_attention)
+
+        attention = self.multi_attention_head(
+            queries=x,
+            keys=x_decoder,
+            values=x_decoder,
+        )
+        x = self.attention_layer_norm(x + attention)
+        return self.mlp_layer_norm(x + self.mlp(x))
