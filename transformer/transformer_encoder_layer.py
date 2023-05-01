8017868b6b1ecf0cf4190cc84789a33251be7571
Grozby <gabriele.aldeghi.27@gmail.com>
2023-05-01 14:13:07 +0100

    feat(TransformerEncoderLayer): allow layer norm before or after
    This is useful for the Visual Transformer paper implementation

--- transformer/transformer_encoder_layer.py
+++ transformer/transformer_encoder_layer.py
@@ -17,15 +17,18 @@ class TransformerEncoderLayer(nn.Module):
         attention_dropout_rate: float = 0.0,
         mlp_dropout_rate: float = 0.0,
         mlp_activation_function: Optional[nn.Module] = None,
+        order_layer_norm: str = "after",
         *args,
         **kwargs,
     ):
+        assert order_layer_norm in ["before", "after"]
         super().__init__(*args, **kwargs)
         self.model_dim = model_dim
         self.mlp_hidden_dim = mlp_hidden_dim
         self.number_attention_heads = number_attention_heads
         self.attention_dropout_rate = attention_dropout_rate
         self.mlp_dropout_rate = mlp_dropout_rate
+        self.order_layer_norm = order_layer_norm
 
         self.multi_attention_head = MultiHeadAttention(
             model_dim=model_dim,
@@ -41,7 +44,7 @@ class TransformerEncoderLayer(nn.Module):
         )
         self.mlp_layer_norm = nn.LayerNorm(model_dim)
 
-    def forward(self, x: torch.Tensor):
+    def forward_layer_norm_after(self, x: torch.Tensor) -> torch.Tensor:
         attention = self.multi_attention_head(
             queries=x,
             keys=x,
@@ -49,3 +52,26 @@ class TransformerEncoderLayer(nn.Module):
         )
         x = self.layer_norm_attention(x + attention)
         return self.mlp_layer_norm(x + self.mlp(x))
+
+    def forward_layer_norm_before(self, x: torch.Tensor) -> torch.Tensor:
+        previous = x
+        x = self.attention_layer_norm(x)
+        x = self.multi_attention_head(
+            queries=x,
+            keys=x,
+            values=x,
+        )
+        previous = x = x + previous
+        x = self.mlp_layer_norm(x)
+        x = self.mlp(x)
+        return x + previous
+
+    def forward(self, x: torch.Tensor):
+        if self.order_layer_norm == "after":
+            return self.forward_layer_norm_after(x)
+        elif self.order_layer_norm == "before":
+            return self.forward_layer_norm_before(x)
+        else:
+            raise NotImplementedError(
+                f"`self.order_layer_norm` can be either `after` or `before`, "
+                f"not {self.order_layer_norm}")
