d78e8c6d446b263cbf6b142d637d6ad7dd4987f9
Grozby <gabriele.aldeghi.27@gmail.com>
2023-05-15 23:41:04 +0100

    fix(ClassToken): repeat only on last two dimensions

--- pytorch/vision_transformer/class_token.py
+++ pytorch/vision_transformer/class_token.py
@@ -19,7 +19,7 @@ class ClassToken(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         batch_size, *_ = x.shape
-        class_token = self.class_token.repeat(batch_size, 1, 1, 1)
+        class_token = self.class_token.repeat(batch_size, 1, 1)
         # Add at the start for each image!
         return torch.cat(
             (class_token, x),
